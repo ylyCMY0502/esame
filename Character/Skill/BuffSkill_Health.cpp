4585700f51811e31e19a10bbd80335a5d11adcd1
ZhaoyaTan <73705102+ylyCMY0502@users.noreply.github.com>
2025-02-12 18:07:23 +0100

    Delete Character directory

--- Character/Skill/BuffSkill_Health.cpp
+++
@@ -1,9 +0,0 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Character/Skill/BuffSkill_Health.h"
-
-void ABuffSkill_Health::ApplyEffect()
-{
-	PlayerReference->ChangeCurrentHP(AddHP);
-}
